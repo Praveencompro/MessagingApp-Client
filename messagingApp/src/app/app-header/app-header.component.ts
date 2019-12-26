64e49ecc4493442111520e14646cb06eb2033929
Praveen <praveen.kumar@comprotechnologies.com>
2019-12-26 17:22:53 +0530

    updating code...

--- messagingApp/src/app/app-header/app-header.component.ts
+++ messagingApp/src/app/app-header/app-header.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class AppHeaderComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(public authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
